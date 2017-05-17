a5e3c6a2d321a1bbc7bd5491891ef30c1dc43ab4
sparksaus <sparksaus@gmail.com>
2017-05-16 22:23:19 -0700

    fixed login ui bug

--- src/java/Login.java
+++ src/java/Login.java
@@ -14,19 +14,13 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Named;
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
- * @author stanchev
+ * @author
  */
-//@Named(value = "login")
-//@SessionScoped
-//@ManagedBean
+@Named(value = "login")
+@SessionScoped
+@ManagedBean
 public class Login implements Serializable {
 
     private int lid;
